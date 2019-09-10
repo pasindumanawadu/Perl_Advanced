51dd01333f6d11bf08a36da57140e6e30f4f189d
pasindumanawadu <pnmanawadu@gmail.com>
2019-09-10 13:21:09 +0530

    Ping Check

--- server_check_ping_advanced.pl
+++ server_check_ping_advanced.pl
@@ -1,4 +1,4 @@
-use 5.0010;
+use 5.010;
 
 use Net::Ping;
 use IO::Socket;
@@ -9,19 +9,25 @@ sub check_alive {
 	my $site_name = $_[0];
 	my $p = Net::Ping->new();
 	if ($p->ping($site_name)) {
-	print 'alive';
+		say 'alive';
+	}
+	else {
+		print 'Not Reachable';
 	}
 	return 0;
-
 }
 
 #check_alive ('perlmaven.com');
-check_alive ('google.com');
+#check_alive ('google.com');
 
 my @sites = ('perlmaven.com','google.com','www.perlmonks.org');
 
 foreach my $a (@sites) {
 	print "$a : ";
-	check_alive($a);
-	print "\n";
+	my $rlt = check_alive($a);
+	if ($rlt = 'Not Reachable'){
+		print "\n";
+		print "\n";
+		}
+	#print "\n";
 }
