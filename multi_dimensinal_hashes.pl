3dcd57997a0ab866d99b2f6ab11c869b3b879e21
pasindumanawadu <pnmanawadu@gmail.com>
2019-09-11 12:13:55 +0530

    2nd Day

---
+++ multi_dimensinal_hashes.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+use Data::Dumper qw (Dumper); 
+
+my %grades;
+$grades{"Foo Bar"}{Mathematics} = 97;
+$grades{"Foo Bar"}{Literature} = 67;
+$grades{"Peti Bar"}{Literature} = 88;
+$grades{"Peti Bar"}{Mathematics} = 82;
+$grades{"Peti Bar"}{Art} = 99;
+
+print Dumper \%grades;
+print "---------------\n";
+
+foreach my $name (sort keys %grades) {
+	foreach my $subject (keys %{$grades{$name}}) {
+		print "$name,$subject:$grades{$name}{$subject}\n";
+		}
+	}
