111684f416b4f2b301455b1c3882fb60c6d95c2f
pasindumanawadu <pnmanawadu@gmail.com>
2019-09-24 13:25:03 +0530

    4th Day

---
+++ 4_autoload.pl
@@ -0,0 +1,16 @@
+# /usr/bin/perl
+use strict;
+use warnings;
+use 5.010;
+use Data::Dumper;
+
+say 'Hi';\
+welcome ('Foo','Bar');
+say 'Bye';
+
+sub AUTOLOAD {
+	our $AUTOLOAD;
+	say $AUTOLOAD;
+	say Dumper \@_;
+}
+
---
+++ 4_subroutines_2.pl
@@ -0,0 +1,30 @@
+# /usr/bin/perl
+use strict;
+use warnings;
+
+my @a = (); 
+my $j = min(@a);
+
+if (defined $j)	{
+	print ("Min of @a is $j\n");
+} else {
+	print ("The array is empty.\n");
+}
+
+my @b = (100,12,31); 
+my $k = min(@b);
+
+if (defined $k)	{
+	print ("Min of @b is $k\n");
+} else {
+	print ("The array is empty.\n");
+}
+
+sub min {
+	my $m = shift;
+	return undef unless defined $m;
+	for (@_) {
+		$m = $_ if $m > $_;
+	}
+	return $m;
+}
