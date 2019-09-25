7932a0ac62dc159dc7a7592381eff90395f42ea0
pasindumanawadu <pnmanawadu@gmail.com>
2019-09-25 09:25:54 +0530

    4th Day

--- Read_Write.pl
+++ Read_Write.pl
@@ -1,17 +1,43 @@
 # !/usr/bin/perl 
-
 use strict;
 use Socket;
 use Try::Tiny;
+my $filename; my $field;
 
 #read from the file
 sub read {
 	my $file = "input.csv";
-	open(F,$ile) || die ("Could not open $file!");
+	open(F,$file) || die ("Could not open $file!");
 }
-
 sub write {
 	open(my $fh, '>>', $filename) or die "Could not open file'$filename'$!";
-	print $th "$field!\n";
+	print $fh "$field!\n";
 }
+
+try {
+	read()
+	while (my $line = <F>) {
+		my($field1,$field2,$field3,$field4) = split ',', $line;
+		print "$field1 : $field2 : $field3 : $field4";
+		write_to_file($field1);
+	}
+	close (F); 
+
+}catch {
+	warn "$_ Please check the file name\n";
+	return 0;
+};
+
+try {
+	my $filename = 'input_wrote.txt';
+	my ($field1) = @_;
+	chomp $field1; 
+	write()
+	close $fh;
+	print "written to file \n";
+
+}catch {
+	warn "$_ Please check the file name\n";
+	return 0;
+};
 
